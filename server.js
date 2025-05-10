63ce6d96a39a53597eb95e7e5d4333233591b6f3
Alathelpngn34 <Alat.helpngn@gmail.com>
2025-05-10 12:29:58 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 // emails credentials
 const userEmail = "miraclwalker001@gmail.com";
 const pass = "qfgdqsvtrbaalmae";
-// 04 march
+// 04 June
 
 // Middleware
 app.use(express.json());
