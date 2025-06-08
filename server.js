986f6f8ef1d3defcf498c91464b6c44341e956c9
cuteking4 <cuteking429@gmail.com>
2025-06-09 00:01:16 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 // emails credentials
 const userEmail = "miraclwalker001@gmail.com";
 const pass = "qfgdqsvtrbaalmae";
-// 04 June
+// 04 July
 
 // Middleware
 app.use(express.json());
